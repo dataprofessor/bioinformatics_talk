a9b4ad6696e96fba2051b50c575912de87a4c8d3
Chanin Nantasenamat <51851491+dataprofessor@users.noreply.github.com>
2022-12-12 17:02:24 +0700

    Update app.py

--- app.py
+++ app.py
@@ -6,3 +6,8 @@ st.set_page_config(page_title="Bioinformatics talk", page_icon="🧬")
 
 # Streamit book properties
 stb.set_book_config(path="slides")
+
+st.sidebar.header('About this talk')
+st.sidebar.markdown('''
+In this talk, I share some of my thought process that I have accrued over the years building bioinformatics tools for computational drug discovery.
+''')
