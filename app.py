f4c4faf6d27fe4dfe5664bc16395500f524a060c
Chanin Nantasenamat <51851491+dataprofessor@users.noreply.github.com>
2022-12-12 16:40:33 +0700

    Update app.py

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ import streamlit as st
 import streamlit_book as stb
 
 # Streamlit webpage properties
-st.set_page_config(page_title="Bioinformatics talk", page_icon="🐦")
+st.set_page_config(page_title="Bioinformatics talk", page_icon="🧬")
 
 # Streamit book properties
 stb.set_book_config(path="slides")
