fcaeaa9722a07eb7c12fffc4c13e9e2901b3ca63
Chanin Nantasenamat <51851491+dataprofessor@users.noreply.github.com>
2022-12-12 17:33:38 +0700

    Update app.py

--- app.py
+++ app.py
@@ -7,12 +7,8 @@ st.set_page_config(page_title="Bioinformatics talk", page_icon="🧬")
 # Streamit book properties
 stb.set_book_config(path="slides")
 
-st.markdown('''
-<style>
-div.block-container.css-12oz5g7.egzxvld2 {
-    margin-top: -5em;
-}
-</style>''', unsafe_allow_html=True)
+with open('style.css') as f:
+    st.markdown(f'<style>{f.read()}</style>', unsafe_allow_html=True)
 
 st.sidebar.header('About this talk')
 st.sidebar.markdown('''
