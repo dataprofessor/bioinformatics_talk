c924c4f18823a05e72e7a3a50b14c26d54debd1b
Chanin Nantasenamat <51851491+dataprofessor@users.noreply.github.com>
2022-12-12 17:41:04 +0700

    Update app.py

--- app.py
+++ app.py
@@ -11,6 +11,11 @@ with open('style.css') as f:
     st.markdown(f'<style>{f.read()}</style>', unsafe_allow_html=True)
 
 st.sidebar.markdown('''
-# About this talk
+# About this Talk
 In this talk, I share some of my thought process that I have accrued over the years building bioinformatics tools for computational drug discovery.
+
+# About the Speaker
+Chanin is a Senior Developer Advocate for Streamlit at Snowflake Inc. In his spare time, he creates educational content on data science, coding and bioinformatics on his YouTube channel [Data Professor](https://youtube.com/dataprofessor).
+
+In his previous career, he worked in academia for 15 years. His recent position was Associate Professor of Bioinformatics and the Head of the Center of Data Mining and Biomedical Informatics, Faculty of Medical Technology, Mahidol University.
 ''')
