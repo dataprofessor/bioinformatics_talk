7aaa25c74592d162cdd8b7500f826ed269c05ea5
Chanin Nantasenamat <51851491+dataprofessor@users.noreply.github.com>
2022-12-12 17:13:20 +0700

    Update app.py

--- app.py
+++ app.py
@@ -1,9 +1,6 @@
 import streamlit as st
 import streamlit_book as stb
 
-with open('style.css') as f:
-    st.markdown(f'<style>{f.read()}</style>', unsafe_allow_html=True)
-
 # Streamlit webpage properties
 st.set_page_config(page_title="Bioinformatics talk", page_icon="🧬")
 
