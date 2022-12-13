5eb28dc276bad6f119f3e73090d5b723b083d8eb
Chanin Nantasenamat <51851491+dataprofessor@users.noreply.github.com>
2022-12-13 14:01:34 +0700

    Update 02.py

--- slides/02.py
+++ slides/02.py
@@ -2,3 +2,9 @@ import streamlit as st
 import streamlit_book as stb
 
 st.header('About Me')
+
+st.markdown('''
+- Senior Developer Advocate
+- Content Creator
+- ex-Associate Professor of Bioinformatics
+''')
